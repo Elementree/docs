3dc1d9abd28481bbb7493dcdcf0cbbe48b7f001c
Christiaan Lombard <base1.christiaan@gmail.com>
2017-02-03 10:57:03 +0200

    some external links

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -19,8 +19,18 @@
     <li>
         <span>External Docs</span>
         <ul>
-            <li><a href="https://laravel.com/api/5.4" target="_blank">Laravel API</a></li>
-            <li><a href="http://commonmark.org/help/" target="_blank">CommonMark - Markdown</a></li>
+            <li><a href="https://laravel.com/docs/5.4" target="_blank"><b>Laravel</b> 5.4 Docs</a></li>
+            <li><a href="https://laravel.com/api/5.4" target="_blank"><b>Laravel</b> 5.4 API Docs</a></li>
+            <li><a href="http://commonmark.org/help/" target="_blank"><b>CommonMark</b> Markdown</a></li>
+
+            <li><a href="https://angular.io/"><b>Angular 2</b></a></li>
+            <li><a href="https://material.angular.io/"><b>Angular 2</b> Material UI</a></li>
+            <li><a href="https://material.io/guidelines/"><b>Google Material</b> Design Guidelines</a></li>
+
+            <li><a href="https://material.io/icons/"><b>Google Material</b> Design Icons</a></li>
+
+
+
         </ul>
     </li>
 
